f23c2d497819363469ef54dcd3b0d1067f1d0ffb
Juan Carlos <juancarlos.rmr@gmail.com>
2017-11-29 21:22:26 +0100

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,15 +15,15 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('Se Crea la app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`El título debería ser 'Exo'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Exo');
   }));
   // it('should render title in a h1 tag', async(() => {
   //   const fixture = TestBed.createComponent(AppComponent);
