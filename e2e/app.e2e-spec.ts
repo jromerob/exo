f55eaf4c340064d297dabfbb090b0a6260e634e4
Juan Carlos Romero <juancromero@grupotecopy.es>
2017-12-04 10:22:25 +0100

    Tests e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-
+import { browser } from 'protractor';
 describe('pagina de inicio', () => {
   let page: AppPage;
 
@@ -11,4 +11,41 @@ describe('pagina de inicio', () => {
     page.navigateTo();
     expect(page.getLogo).not.toBeNull();
   });
+
+});
+
+describe('pagina de consultores', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('Muestra consultores', () => {
+    page.navigateTo('/consultores');
+    expect(page.getConsultantsCard).not.toBeNull();
+  });
+});
+
+describe('pagina detalle de consultor con ID=1', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('Visualiza consultor 1', () => {
+    page.navigateTo('/consultor/1');
+    expect(page.getConsultantsCard).not.toBeNull();
+  });
+
+  it('Muestra el formulario de añadir comentario en el click del botón', () => {
+    page.navigateTo('/consultor/1');
+    const button = page.getNewCommentButton();
+    button.click().then(() => {
+      expect(page.getNewCommentForm).not.toBeNull();
+    });
+
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,26 @@
-import { browser, by, element } from 'protractor';
+
+import { browser, element, by, By, $, $$, ExpectedConditions } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(page?: string) {
+    if (!page) { page = '/'; }
+    return browser.get(page);
   }
 
   getLogo() {
     return element(by.css('img.homeLogo'));
+  }
+
+  getConsultantsCard() {
+    return element(by.css('mat-card.consultant'));
+  }
+  getNewCommentButton() {
+    return element(by.id('addCommentButton'));
+  }
+
+  getNewCommentForm() {
+    browser.debugger();
+    console.log(element(by.id('addComment')));
+    return element(by.id('addComment'));
   }
 }
